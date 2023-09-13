e99b3ede0aa85360637f8dd51f651adde6deab8b
phuproz56 <phuproz348@gmail.com>
2023-09-13 19:02:37 +0700

    update layout admin dashboard date: 13/9/2023

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { getNavs } from "../navigation";
+import { BiLogInCircle } from "react-icons/bi";
 
-function Sidebar() {
+function Sidebar({ showSidebar, setShowSidebar }) {
   const { pathname } = useLocation();
   const [allNav, setAllNav] = useState([]);
   useEffect(() => {
@@ -11,9 +12,16 @@ function Sidebar() {
   }, []);
   return (
     <div>
-      <div></div>
       <div
-        className={`w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+        onClick={() => setShowSidebar(false)}
+        className={`fixed duration-200 ${
+          !showSidebar ? "invisible" : "visible"
+        } w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}
+      ></div>
+      <div
+        className={`w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
+          showSidebar ? "left-0" : "-left-[260px] lg:left-0"
+        }`}
       >
         <div className="h-[70px] flex justify-center items-center">
           <Link to="/" className="w-[180px] h-[50px]">
@@ -24,7 +32,8 @@ function Sidebar() {
           <ul>
             {allNav.map((n, i) => (
               <li key={i}>
-                <Link to={n.path}
+                <Link
+                  to={n.path}
                   className={`${
                     pathname === n.path
                       ? "bg-slate-600 shadow-indigo-500/30 text-white duration-500"
@@ -36,6 +45,17 @@ function Sidebar() {
                 </Link>
               </li>
             ))}
+            <li>
+              <button
+                className="text-[#d0d2d6] font-normal duration-200
+                   px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1"
+              >
+                <span>
+                  <BiLogInCircle />
+                </span>
+                <span>Logout</span>
+              </button>
+            </li>
           </ul>
         </div>
       </div>
