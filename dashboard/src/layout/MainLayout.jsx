9036f22c6b278733967f3f64521399c2b30773e0
phuproz56 <phuproz348@gmail.com>
2023-09-13 18:43:46 +0700

    update layout admin dashboard date: 13/9/2023

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function MainLayout() {
   return (
-    <div>MainLayout</div>
-  )
+    <div className="bg-[#161d31] w-full min-h-screen">
+      <Header />
+      <Sidebar />
+      <div className="ml-0 lg:ml-[260px] pt-[95px] transition-all">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default MainLayout
+export default MainLayout;
