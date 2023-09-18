c70263a532cbbf3291419c17ba5f38227504ec6c
phuproz56 <phuproz348@gmail.com>
2023-09-18 13:19:52 +0700

    update backend 18/9

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,4 +1,6 @@
 const adminModel = require("../models/adminModel");
+const sellerModel = require("../models/sellerModel");
+const sellerCustomerModel = require("../models/chat/sellerCustomerModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { responseReturn } = require("../utils/response");
@@ -29,6 +31,65 @@ class authControllers {
       responseReturn(res, 500, { error: error.message });
     }
   };
+
+  seller_login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const seller = await sellerModel.findOne({ email }).select("+password");
+      if (seller) {
+        const match = await bcrypt.compare(password, seller.password);
+        if (match) {
+          const token = await createToken({
+            id: seller.id,
+            role: seller.role,
+          });
+          res.cookie("accessToken", token, {
+            exprires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login success!" });
+        } else {
+          responseReturn(res, 400, { error: "password wrong!" });
+        }
+      } else {
+        responseReturn(res, 400, { error: "email not found" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
+
+  seller_register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      const getUser = await sellerModel.findOne({ email });
+      if (getUser) {
+        responseReturn(res, 404, { error: "Email already exit" });
+      } else {
+        const seller = await sellerModel.create({
+          name,
+          email,
+          password: await bcrypt.hash(password, 10),
+          method: "menualy",
+          shopInfo: {},
+        });
+        await sellerCustomerModel.create({
+          myId: seller.id,
+        });
+        const token = await createToken({
+          id: seller.id,
+          role: seller.role,
+        });
+        res.cookie("accessToken", token, {
+          exprires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+
+        responseReturn(res, 201, { token, message: "register success" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: "Internal server error" });
+    }
+  };
+
   getUser = async (req, res) => {
     const { id, role } = req;
     try {
