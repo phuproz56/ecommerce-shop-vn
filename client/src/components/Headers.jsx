e0c64b0ed3900be595bb7f9c5ae65debdb59c560
Tran Kha Phu B1900354 <phuproz348@gmail.com>
2023-09-23 20:04:01 +0700

    update header client

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { GrMail } from "react-icons/gr";
+import { IoIosCall } from "react-icons/io";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import {
   FaFacebookF,
@@ -8,14 +9,23 @@ import {
   FaLock,
   FaList,
 } from "react-icons/fa";
-import { AiOutlineTwitter, AiFillGithub } from "react-icons/ai";
+import {
+  AiOutlineTwitter,
+  AiFillGithub,
+  AiFillHeart,
+  AiFillShopping,
+} from "react-icons/ai";
 import { Link, useLocation } from "react-router-dom";
 
 const Headers = () => {
+  const [categoryShow, setCategoryShow] = useState(true);
   const { pathname } = useLocation();
   const [showShidebar, setShowShidebar] = useState(true);
   const user = 1;
-  console.log(pathname);
+  const wishlist = 4;
+  const categorys = ["Clothing", "Sports", "Shose", "Laptop", "Tablet"];
+  const [searchValue, setSearchValue] = useState("");
+  const [category, setCategory] = useState("");
   return (
     <div className="w-full bg-white">
       <div className="header-top bg-[#eeeeee] md-lg:hidden">
@@ -122,7 +132,9 @@ const Headers = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/shop" ? "text-[#7fad39]" : "text-slate-600"
+                        pathname === "/shop"
+                          ? "text-[#7fad39]"
+                          : "text-slate-600"
                       }`}
                     >
                       Shop
@@ -131,7 +143,9 @@ const Headers = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/blog" ? "text-[#7fad39]" : "text-slate-600"
+                        pathname === "/blog"
+                          ? "text-[#7fad39]"
+                          : "text-slate-600"
                       }`}
                     >
                       Blog
@@ -140,21 +154,271 @@ const Headers = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/about" ? "text-[#7fad39]" : "text-slate-600"
+                        pathname === "/about"
+                          ? "text-[#7fad39]"
+                          : "text-slate-600"
                       }`}
                     >
                       About
                     </Link>
-                  </li><li>
+                  </li>
+                  <li>
                     <Link
-                      className={`p-2 block ${  
-                        pathname === "/contact" ? "text-[#7fad39]" : "text-slate-600"
+                      className={`p-2 block ${
+                        pathname === "/contact"
+                          ? "text-[#7fad39]"
+                          : "text-slate-600"
                       }`}
                     >
                       Contact
                     </Link>
                   </li>
                 </ul>
+                <div className="flex md-lg:hidden justify-center items-center gap-5">
+                  <div className="flex justify-center gap-5">
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-red-500">
+                        <AiFillHeart />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-green-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
+                        {wishlist}
+                      </div>
+                    </div>
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-orange-500">
+                        <AiFillShopping />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-green-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]">
+                        {wishlist}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden md-lg:block">
+        <div
+          onClick={() => setShowShidebar(true)}
+          className={`fixed duration-200 transition-all ${
+            showShidebar ? "invisible" : "visible"
+          } hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20`}
+        ></div>
+        <div
+          className={`w-[300px] z-[9999] transition-all duration-200 fixed ${
+            showShidebar ? "-left-[300px]" : "left-0 "
+          } top-0 overflow-y-auto bg-white h-screen py-6 px-8`}
+        >
+          <div className="flex justify-start flex-col gap-6">
+            <Link to="/">
+              <img src="/images/logo.png" alt="logo" />
+            </Link>
+            <div className="flex justify-start items-center gap-10">
+              <div
+                className="flex group cursor-pointer text-slate-800 text-sm
+                justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]
+                after:absolute before:absolute"
+              >
+                <img src="/images/language.png" alt="" />
+                <span>
+                  <MdOutlineKeyboardArrowDown />
+                </span>
+                <ul className="absolute invisible transition-all-to-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10">
+                  <li>VietNam</li>
+                  <li>English</li>
+                </ul>
+              </div>
+              {user ? (
+                <Link
+                  className="flex cursor-pointer justify-center items-center gap-2 text-sm"
+                  to="/dashboard"
+                >
+                  <span>
+                    <FaUser />
+                  </span>
+                  <span>Phu</span>
+                </Link>
+              ) : (
+                <div className="flex cursor-pointer justify-center items-center gap-2 text-sm">
+                  <span>
+                    <FaLock />
+                  </span>
+                  <span>Login</span>
+                </div>
+              )}
+            </div>{" "}
+            <ul className="flex flex-col justify-start items-start text-md font-semibold uppercase">
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/" ? "text-[#7fad39]" : "text-slate-600"
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/shop" ? "text-[#7fad39]" : "text-slate-600"
+                  }`}
+                >
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/blog" ? "text-[#7fad39]" : "text-slate-600"
+                  }`}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/about" ? "text-[#7fad39]" : "text-slate-600"
+                  }`}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/contact"
+                      ? "text-[#7fad39]"
+                      : "text-slate-600"
+                  }`}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className="flex justify-start items-center gap-4">
+              <a href="#">
+                <FaFacebookF />
+              </a>
+              <a href="#">
+                <AiOutlineTwitter />
+              </a>
+              <a href="#">
+                <FaLinkedinIn />
+              </a>
+              <a href="#">
+                <AiFillGithub />
+              </a>
+            </div>
+            <div className="w-full flex justify-end md-lg:justify-start gap-3 items-center">
+              <div className="w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center">
+                <span>
+                  <IoIosCall />
+                </span>
+              </div>
+              <div className="flex justify-end flex-col gap-1">
+                <h2 className="text-sm font-medium text-slate-700">
+                  +1234567890
+                </h2>
+                <span className="text-xs">support 24/7 time</span>
+              </div>
+            </div>
+            <ul className="flex flex-col justify-start items-start gap-3 text-[#1c1c1c]">
+              <li className="flex justify-start items-center gap-2 text-sm">
+                <span>
+                  <GrMail />
+                </span>
+                <span>support.shopvn@gmail.com</span>
+              </li>
+              <span className="text-sm">Shop-VN</span>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-[85%] lg:w-[90%] mx-auto">
+        <div className="flex w-full flex-wrap md-lg:gap-8">
+          <div className="w-3/12 md-lg:w-full">
+            <div className="bg-white relative">
+              <div
+                onClick={() => setCategoryShow(!categoryShow)}
+                className="h-[50px] bg-violet-400 text-white flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-bold text-md cursor-pointer"
+              >
+                <div className="flex justify-center items-center gap-3">
+                  <span>
+                    <FaList />
+                  </span>
+                  <span>All Category</span>
+                </div>
+                <span className="pt-1">
+                  <MdOutlineKeyboardArrowDown />
+                </span>
+              </div>
+              <div
+                className={`${
+                  categoryShow
+                    ? "h-0"
+                    : "h-[400px] overflow-hidden transition-all md-lg:relative duration-500 absolute z-[9999] bg-white w-full border-x"
+                }`}
+              >
+                <ul className="py-2 text-slate-600 font-medium">
+                  {categorys.map((c, i) => {
+                    return (
+                      <li
+                        key={i}
+                        className="flex justify-start items-center gap-2 px-[24px] py-[6px]"
+                      >
+                        <Link className="text-sm block">{c}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="w-9/12 pl-8 md-lg:pl-0 md-lg:w-full">
+            <div className="flex flex-wrap w-full justify-between items-center md-lg:gap-6">
+              <div className="w-8/12 md-lg:w-full">
+                <div className="flex border h-[50px] items-center relative gap-5">
+                  <div className="relative after:absolute after:h-[25px] after:w-[1px] after:bg-[#afafaf] after:-right-[15px] md:hidden">
+                    <select
+                      onChange={(e) => setCategory(e.target.value)}
+                      className="w-[150px] text-slate-600 font-semibold bg-transparent px-2 h-full outline-0 border-none"
+                      name=""
+                      id=""
+                    >
+                      <option value="">Select category</option>
+                      {categorys.map((c, i) => (
+                        <option value={c}>{c}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <input
+                    className="w-full relative bg-transparent text-slate-500 outline-0 px-3 h-full"
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    type="text"
+                    placeholder="what do you need?"
+                  />
+                  <button className="bg-violet-400 right-0 absolute px-8 h-full font-semibold uppercase text-white">
+                    Search
+                  </button>
+                </div>
+              </div>
+              <div className="w-4/12 block md-lg:hidden pl-2 md-lg:w-full md-lg:pl-0">
+                <div className="w-full flex justify-end md-lg:justify-start gap-3 items-center">
+                  <div className="w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center">
+                    <span>
+                      <IoIosCall />
+                    </span>
+                  </div>
+                  <div className="flex justify-end flex-col gap-1">
+                    <h2 className="text-sm font-medium text-slate-700">
+                      +1234567890
+                    </h2>
+                    <span className="text-xs">support 24/7 time</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
