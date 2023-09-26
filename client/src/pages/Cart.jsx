9d38df43703388c6cd831798c00e1a3b9dd7bd07
Tran Kha Phu B1900354 <phuproz348@gmail.com>
2023-09-26 18:51:45 +0700

    update cart tailwincss frontend

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -86,8 +86,83 @@ const Cart = () => {
                         ))}
                       </div>
                     ))}
+                    {outOfStockProduct.length > 0 && (
+                      <div className="flex flex-col gap-3">
+                        <div className="bg-white p-4">
+                          <h2 className="text-md text-red-500 font-semibold">
+                            Out of Stock {outOfStockProduct.length}
+                          </h2>
+                        </div>
+                        <div className="bg-white p-4">
+                        {[1, 2].map((p, i) => (
+                          <div className="w-full flex flex-wrap">
+                            <div className="flex sm-w-full gap-2 w-7/12">
+                              <div className="flex gap-2 justify-start items-center">
+                                <img
+                                  className="w-[80px] h-[80px]"
+                                  src={`http://localhost:3000/images/products/${
+                                    i + 1
+                                  }.webp`}
+                                  alt="product_image"
+                                />
+                                <div className="pr-4 text-slate-600">
+                                  <h2 className="text-md">Ao thun askdjk</h2>
+                                  <span className="text-sm">Brand : easy</span>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="flex justify-between w-5/12 sm:w-full sm:mt-3 ">
+                              <div className="pl-4 sm:pt-0">
+                                <h2 className="text-orange-500 text-lg">
+                                  $135
+                                </h2>
+                                <p className="line-through">$150</p>
+                                <p>-10%</p>
+                              </div>
+                              <div className="flex gap-2 flex-col">
+                                <div className="flex bg-slate-200 h-[30px] justify-center items-center text-xl">
+                                  <div className="px-3 cursor-pointer">-</div>
+                                  <div className="px-3">5</div>
+                                  <div className="px-3 cursor-pointer">+</div>
+                                </div>
+                                <button className="px-5 py-[3px] bg-red-500 text-white">
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
+              </div>
+              <div className="w-[33%] md-lg:w-full ">
+                                  <div className="pl-3 md-lg:pl-0 md-lg:mt-5">
+                                    {
+                                      cart_products.length>0 && <div className="bg-white p-3 text-slate-600 flex flex-col gap-3">
+                                        <h2 className="text-xl font-bold">Order Sumary</h2>
+                                        <div className="flex justify-between  items-center">
+                                          <span>4 Items</span>
+                                          <span>$135</span>
+                                        </div>
+                                        <div className="flex justify-between  items-center">
+                                          <span>Shiping Fee</span>
+                                          <span>$1</span>
+                                        </div>
+                                        <div className="flex gap-2">
+                                          <input className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-green-500" type="text" placeholder="Input Voucher Coupon" />
+                                          <button className="px-5 py-[1px] bg-blue-500 text-white rounded-sm uppercase text-sm">Apply</button>
+                                        </div>
+                                        <div className="flex justify-between  items-center">
+                                          <span>Total</span>
+                                          <span className="text-lg text-orange-500">$136</span>
+                                        </div>
+                                        <button className="px-5 py-[6px] rounded-sm hover:shadow-orange-500/20 hover:shadow-lg bg-orange-500 text-sm text-white uppercase">Proceed to checkout 4</button>
+                                      </div>
+                                    }
+                                  </div>
               </div>
             </div>
           ) : (
