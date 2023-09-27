355ddfb2d5847af4348b2cb9f22dcc2818046c07
Tran Kha Phu B1900354 <phuproz348@gmail.com>
2023-09-27 16:21:30 +0700

    update login.jsx, register.jsx tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,20 @@ import Home from "./pages/Home";
 import Shops from "./pages/Shops";
 import Cart from "./pages/Cart";
 import Details from "./pages/Details";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Shipping from "./pages/Shipping";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/shops" element={<Shops />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/shipping" element={<Shipping />}></Route>
         <Route path="/product/details/:slug" element={<Details />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Shipping.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Headers from "../components/Headers";
+import Footer from "../components/Footer";
+
+const Shipping = () => {
+  return (
+    <div>
+      <Headers />
+      <Footer />
+    </div>
+  );
+};
+
+export default Shipping;
